e7f34831ad17e6846e88bc8dab8a48de687aecaf
Roberto Cenci <rob.cenci.72@gmail.com>
2017-04-28 12:49:13 +0000

    Make a basic User model (including secure passwords)

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -8,5 +8,6 @@ class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
     assert_equal full_title,  @base_title
     assert_equal full_title("Help"), "Help | #{@base_title}"
+    assert_equal full_title("Sign up"), "Sign up | #{@base_title}"
   end
 end
