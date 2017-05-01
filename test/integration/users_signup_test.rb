a992923ea7416eaf1c245922d9c4668ef6a32d55
Roberto Cenci <rob.cenci.72@gmail.com>
2017-05-01 10:10:55 +0000

    Use SSL and Unicorn in production

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post users_path, user: { name: "", email: "user@invalid", password: "foo", password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    #assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div#error_explanation'
+    #assert_select 'div.<CSS class for field with error>'
+    assert_select 'div.field_with_errors'
   end  
   
   test "valid signup information" do
@@ -19,5 +23,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post_via_redirect users_path, user: { name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash == nil
   end  
 end
